43b31f39218b1d094048415a18b089f4a794325e
mahabbat <zakariyayevmahabbat@gmail.com>
2022-08-03 08:30:21 +0400

    1

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,15 @@
+
+package javaapplication1;
+import java.util.Scanner;
+public class JavaApplication1 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        math.topla(5, 4);
+        
+    }
+    
+}
---
+++ JavaApplication1/src/javaapplication1/math.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+
+
+/**
+ *
+ * @author Mahabbat
+ */
+public class math {
+    
+    public static void topla(int a, int b) {
+            System.out.println(a+b);
+        
+    }
+ 
+   
+}
